ae62feab4268e18f2786799d97096fc96206f210
PavlovaElena <sferati@mail.ru>
2022-06-21 05:08:26 +0300

    Setup interface

---
+++ AnimationApp/Models/Animation.swift
@@ -0,0 +1,8 @@
+//
+//  Animation.swift
+//  AnimationApp
+//
+//  Created by Elena Pavlova on 21.06.2022.
+//
+
+import Foundation
---
+++ AnimationApp/ViewControllers/AnimationViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Elena Pavlova on 21.06.2022.
+//
+
+import UIKit
+
+class AnimationViewController: UIViewController {
+
+    @IBOutlet weak var animationDescriptionLabel: UILabel!
+    @IBOutlet weak var animationView: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func runAnimationPressed(_ sender: Any) {
+        
+    }
+}
+
